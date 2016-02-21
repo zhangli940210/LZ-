4e17aa658123388fffa3fec3f3beed0ad36d10cd
gj <gj@itcast.cn>
2016-02-21 13:52:59 +0800

    初始化项目

---
+++ My简单通讯录实现/My简单通讯录实现/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My简单通讯录实现
+//
+//  Created by apple on 16/2/21.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My简单通讯录实现/My简单通讯录实现/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  My简单通讯录实现
+//
+//  Created by apple on 16/2/21.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
